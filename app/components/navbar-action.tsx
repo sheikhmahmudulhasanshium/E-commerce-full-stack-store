d79d58c05bcb71c25d6459fd4af7917f5570ff3d
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-04 01:45:04 +0600

    navbar completed for now

--- app/components/navbar-action.tsx
+++ app/components/navbar-action.tsx
@@ -1,7 +1,27 @@
 "use client"
+
+import { ShoppingBag } from "lucide-react";
+import Button from "./ui/button";
+import { useEffect, useState } from "react";
+
+
 const NavbarActions = () => {
+//our cart will use local storage to preserve the amount of item in the cart. to prevent hydration mount will be used 
+const [isMounted,setIsMounted]=useState(false)
+useEffect(()=>{
+    setIsMounted(true)
+},[])
+
+    if(!isMounted){
+        return null
+    }
     return ( 
-        <div className="ml-auto flex-center gap-x-4">Hello Actions</div>
+        <div className="ml-auto flex-center gap-x-4">
+            <Button className="flex items-center rounded-full bg-black px-4 py-2">
+                <ShoppingBag size={20} color="white"/>
+                <span className="ml-2 text-sm font-medium text-white">0</span>
+            </Button>
+        </div>
      );
 }
  
--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>{
@@ -12,9 +13,10 @@ const Button=forwardRef<HTMLButtonElement,ButtonProps>(({
     ...props
 },ref)=>{
     return (
-        <button ref={ref}>
+        <button ref={ref} className={cn("w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition",className)}>
             {children}
         </button>
     )
 })
 Button.displayName="Button"
+export default Button
