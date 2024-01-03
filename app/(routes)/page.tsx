1bf39f90b6fa6f7b4b6f38e61c8d2dc8f67e7350
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-04 02:33:24 +0600

    billboard added

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,16 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboards";
+import BillBoard from "../components/billboard";
+import Container from "../components/ui/container";
+//prevent caching
+export const revalidate=0
+const HomePage = async() => {
+    const billboard=await getBillboard("c26860b0-4f30-41ec-98ab-b8308f376f83")
     return ( 
-        <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <BillBoard data={billboard}/>
+            </div>
+        </Container>
      );
 }
  
