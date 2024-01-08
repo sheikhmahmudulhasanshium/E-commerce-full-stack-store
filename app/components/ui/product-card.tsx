08899e0acaa6bd4f84d5e8e099caf925bb8856e7
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-09 02:28:02 +0600

    modal,preview, provider, toast, cart etc functionality added

--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -6,14 +6,27 @@ import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product
 }
 const ProductCard: React.FC<ProductCard> = ({data}) => {
+    const cart=useCart()
+    const previewModal=usePreviewModal()
     const router=useRouter()
     const handleClick=()=>{
         router.push(`/product/${data?.id}`)
+    }
+    const onPreview:MouseEventHandler<HTMLButtonElement>=(event)=>{
+        event.stopPropagation()
+        previewModal.onOpen(data)
+
+    }
+    const onAddToCart:MouseEventHandler<HTMLButtonElement>=(event)=>{
+        cart.addItem(data)
     }
     return ( 
         <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -22,8 +35,8 @@ const ProductCard: React.FC<ProductCard> = ({data}) => {
                 <Image src={data?.images?.[0]?.url} fill alt="Image" className="aspect-square object-cover rounded-md"/>
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
-                        <IconButton onClick={()=>{}} icon={<Expand size={20} className="text-gray-500"/>}/>
-                        <IconButton onClick={()=>{}} icon={<ShoppingCart size={20} className="text-gray-500"/>}/>
+                        <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-500"/>}/>
+                        <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-gray-500"/>}/>
 
                     </div>
                 </div>
