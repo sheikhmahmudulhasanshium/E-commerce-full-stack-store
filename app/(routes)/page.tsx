128e6103ed95808fb615f858bf576a9a4e5e574c
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-08 19:22:53 +0600

    Categories entity and filter added

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,11 +9,12 @@ const HomePage = async() => {
     const products=await getProducts({isFeatured:true})
     const billboard=await getBillboard("9825ba90-b41d-4f2c-a22e-9368b2e8d964")
     //console.log(billboard)
+    
     return ( 
         <Container>
             <div className="space-y-10 pb-10">
                 <BillBoard data={billboard}/>
-            
+                
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
                     <ProductList title="Featured Products" items={products}/>
                 </div>
