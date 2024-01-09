300b37ecbc430655327abaeb28910873b4643a05
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-10 04:24:28 +0600

    vercel error debug attempt-5

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,26 +1,29 @@
 import getBillboard from "@/actions/get-billboards";
-import BillBoard from "../components/ui/billboard";
+import Billboard from "../components/ui/billboard";
 import Container from "../components/ui/container";
 import getProducts from "@/actions/get-products";
 import ProductList from "../components/product-list";
 //prevent caching
-export const revalidate=0
-const HomePage = async() => {
-    const products=await getProducts({isFeatured:true})
-    const billboard=await getBillboard("9825ba90-b41d-4f2c-a22e-9368b2e8d964")
-    //console.log(billboard)
-    
-    return ( 
-        <Container>
-            <div className="space-y-10 pb-10">
-                <BillBoard data={billboard}/>
-                
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products}/>
-                </div>
-            </div>
-        </Container>
-     );
-}
- 
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("9825ba90-b41d-4f2c-a22e-9368b2e8d964");
+      //console.log(billboard)
+
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
 export default HomePage;
--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -5,35 +5,44 @@ import Info from "@/app/components/info";
 import ProductList from "@/app/components/product-list";
 import Container from "@/app/components/ui/container";
 
-interface ProductPageProps{
-    params:{
-        productId:string
-    }
+export const revalidate = 0;
+
+interface ProductPageProps {
+  params: {
+    productId: string;
+  },
 }
-const ProductPage:React.FC<ProductPageProps> = async({params}) => {
-    {/**Get individual product after clicking expand */}
-    const product=await getProduct(params.productId)
-    const suggestedProducts=await getProducts({
-        categoryId:product?.category?.id
-    })
-    return ( 
-        <div className="bg-white">
-            <Container>
-                <div className="px-4 py-10 sm:px-6 lg:px-8">
-                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        {/**Gallery */}
-                        <Gallery images={product.images }/>
-                        <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                        {/**Info */}
-                        <Info data={product}/>
-                        </div>
-                        <hr className="my-10"/>
-                        <ProductList title="Related Items" items={suggestedProducts}/>
-                    </div>
-                </div>
-            </Container>
+
+const ProductPage: React.FC<ProductPageProps> = async ({ 
+  params
+ }) => {
+  const product = await getProduct(params.productId);
+  const suggestedProducts = await getProducts({ 
+    categoryId: product?.category?.id
+  });
+
+  if (!product) {
+    return null;
+  }
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/**Gallery */}
+            <Gallery images={product.images} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                        {/**Info */}            
+              <Info data={product} />
+            </div>
+          </div>
+          <hr className="my-10" />
+          <ProductList title="Related Items" items={suggestedProducts} />
         </div>
-    );
+      </Container>
+    </div>  
+  )
 }
- 
+
 export default ProductPage;
