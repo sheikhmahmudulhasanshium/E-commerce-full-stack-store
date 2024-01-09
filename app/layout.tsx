1ac9c7dbccec301c730bc364a3171ae3de20fd41
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-10 04:44:50 +0600

    vercel error debug attempt-6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
-import './globals.css'
-import Footer from './components/footer'
-import Navbar from './components/navbar'
+
 import ModalProvider from '@/providers/modal-provider'
 import ToastProvider from '@/providers/toast-provider'
 
+import './globals.css'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+
 const font = Urbanist({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Store App',
-  description: 'Store app',
+export const metadata = {
+  title: 'Store',
+  description: 'Store - The place for all your purchases.',
 }
 
 export default function RootLayout({
@@ -21,11 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <ModalProvider/>
-        <ToastProvider/>
-        <Navbar/>
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   )
