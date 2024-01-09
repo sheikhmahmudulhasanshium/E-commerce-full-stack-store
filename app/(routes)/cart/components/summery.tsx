acfff378c3dc143cb3059048ea6e0d29fce034e2
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-10 03:41:05 +0600

    vercel error debug attempt-3

--- app/(routes)/cart/components/summery.tsx
+++ app/(routes)/cart/components/summery.tsx
@@ -1,52 +1,59 @@
-"use client"
-import axios from "axios"
-import { useEffect } from "react"
-import { useSearchParams } from "next/navigation"
-import useCart from '@/hooks/use-cart'
-import {toast} from 'react-hot-toast'
-import Currency from "@/app/components/ui/currency"
-import Button from "@/app/components/ui/button"
+"use client";
 
-const Summery=()=>{
-    const searchParams=useSearchParams()
-    const items=useCart((state)=>state.items)
-    const removeAll=useCart((state)=>state.removeAll)
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import useCart from "@/hooks/use-cart";
+import { toast } from "react-hot-toast";
+import Currency from "@/app/components/ui/currency";
+import Button from "@/app/components/ui/button";
 
-    const totalPrice=items.reduce((total,item)=>{
-        return total+Number(item.price)
-    },0)//default total=0
-    //if checkout is complete
-    useEffect(()=>{
-        if(searchParams.get("success")){
-            toast.success("Payment completed.")
-            removeAll()
-        }
-        if(searchParams.get("canceled")){
-            toast.error("Something went wrong.")
-        }
-    },[searchParams,removeAll])
-    const onCheckOut= async()=>{
-        const response=await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`,{
-            //data
-            productIds: items.map((item)=>item.id)
-        })
-        window.location=response.data.url
+const Summary = () => {
+  const searchParams = useSearchParams();
+  const items = useCart((state) => state.items);
+  const removeAll = useCart((state) => state.removeAll);
+
+  useEffect(() => {
+    if (searchParams.get('success')) {
+      toast.success('Payment completed.');
+      removeAll();
     }
 
-    return(
-        <div className=" mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-            <h2 className="text-lg font-medium text-gray-950">Order Summmery</h2>
-            <div className="mt-6 space-y-4">
-                <div className="flex items-center justify-between border-t border-gray-300 pt-2">
-                    <div className="text-base font-medium text-gray-900">
-                        Order total
-                    </div>
-                    <Currency value={totalPrice}/>
+    if (searchParams.get('canceled')) {
+      toast.error('Something went wrong.');
+    }
+  }, [searchParams, removeAll]);
 
-                </div>
-            </div>
-            <Button disabled={items.length===0} className="w-full mt-6" onClick={onCheckOut}>Checkout</Button>
+  const totalPrice = items.reduce((total, item) => {
+    return total + Number(item.price)
+  }, 0);//default total=0
+
+  const onCheckout = async () => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+      productIds: items.map((item) => item.id)
+    });
+
+    window.location = response.data.url;
+  }
+
+  return ( 
+    <div
+      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+    >
+      <h2 className="text-lg font-medium text-gray-900">
+        Order summary
+      </h2>
+      <div className="mt-6 space-y-4">
+        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+          <div className="text-base font-medium text-gray-900">Order total</div>
+         <Currency value={totalPrice} />
         </div>
-    )
+      </div>
+      <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
+        Checkout
+      </Button>
+    </div>
+  );
 }
-export default Summery
+ 
+export default Summary;
