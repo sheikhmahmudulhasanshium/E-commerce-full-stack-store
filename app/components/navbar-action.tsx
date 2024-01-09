fa8588e32f50de6985131c49ec61b5f5e5f8cd63
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-10 04:04:56 +0600

    vercel error debug attempt-4

--- app/components/navbar-action.tsx
+++ app/components/navbar-action.tsx
@@ -9,25 +9,30 @@ import Button from "./ui/button";
 
 const NavbarActions = () => {
 //our cart will use local storage to preserve the amount of item in the cart. to prevent hydration mount will be used 
-const [isMounted,setIsMounted]=useState(false)
-useEffect(()=>{
-    setIsMounted(true)
-},[])
+const [isMounted, setIsMounted] = useState(false);
 
-    const router=useRouter()
-    const cart=useCart()
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if(!isMounted){
-        return null
-    }
-    return ( 
-        <div className="ml-auto flex-center gap-x-4">
-            <Button onClick={()=>router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
-                <ShoppingBag size={20} color="white"/>
-                <span className="ml-2 text-sm font-medium text-white">{cart.items.length}</span>
-            </Button>
-        </div>
-     );
+  const router = useRouter();
+  const cart = useCart();
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return ( 
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag size={20} color="white"
+        />
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
+      </Button>
+    </div>
+  );
 }
  
 export default NavbarActions;
